242a8a873457103892cd9081c870b99b6824a9f5
Rafael Cruz <rcas2009@outlook.com>
2024-05-27 11:12:49 -0300

    Criando o CRUD

--- view.py
+++ view.py
@@ -3,31 +3,54 @@ import sqlite3 as lite
 # Conectando ao db
 con = lite.connect('dados.db')
 
-dados = ['vaso', 'sala', 'vaso teste', 'vasomarca', '27052024', '25.99', 'series', 'local_imagem.png']
 # Inserir dados
-with con:
-    cur = con.cursor()
-    query = "INSERT INTO inventario(nome, local, descricao, marca, data, valor, serie, imagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-    cur.execute(query, dados)
+def inserir_dados(dados):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO inventario(nome, local, descricao, marca, data, valor, serie, imagem) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+        cur.execute(query, dados)
 
-atualizar_dados = ['sofa', 'sala', 'sofa teste', 'sofamarca', '27052024', '27.99', 'series', 'local_imagem.png', 1]
 # Atualizar dados
-with con:
-    cur = con.cursor()
-    query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data=?, valor=?, serie=?, imagem=? WHERE id=?"
-    cur.execute(query, atualizar_dados)
+def atualizar_dados(dados):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data=?, valor=?, serie=?, imagem=? WHERE id=?"
+        cur.execute(query, dados)
 
-# Ver dados
-ver_dados = []
-with con:
-    cur = con.cursor()
-    query = "SELECT * FROM inventario"
-    cur.execute(query)
+# Deletar dados
+def deletar_dados(id):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM inventario WHERE id=?"
+        cur.execute(query, id)
 
-    # Pega todas as linhas da tabela
-    rows = cur.fetchall()
-    # Para cada linha, adiciona a linha à lista de dados
-    for row in rows:
-        ver_dados.append(row)
+# Ver todos os dados
+def ver_todos_dados():
+    ver_dados = []
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventario"
+        cur.execute(query)
 
-print(ver_dados)
+        # Pega todas as linhas da tabela
+        rows = cur.fetchall()
+        # Para cada linha, adiciona a linha à lista de dados
+        for row in rows:
+            ver_dados.append(row)
+    return ver_dados
+
+
+# Ver dados individuais
+def ver_dados_individuais(id):
+    ver_dados_individuais = []
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventario WHERE id=?"
+        cur.execute(query, id)
+
+        rows = cur.fetchall()
+        for row in rows:
+            ver_dados_individuais.append(row)
+
+
+
